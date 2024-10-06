4498bd8bbc7f8c94fa08452c20b6e63a78a564f7
otavio <proj.otaviocf@gmail.com>
2024-10-06 16:56:12 -0300

    implemented function to mark a task as done

--- typicode-todos/script.js
+++ typicode-todos/script.js
@@ -43,15 +43,41 @@ function removeTask(task) {
   task.parentElement.parentElement.parentElement.remove()
 }
 
-function taskHandler(event) {
-  const target = event.target
-  if (event.target.id === 'trash') {
-    removeTask(target)
-  } else if (event.target.id === 'check') {
+function markAsDone(task) {
+  // Remove from list of incomplete tasks
+  removeTask(task)
 
+  // Create task element
+  const div = document.createElement('div')
+  div.classList.add('task')
+  const p = document.createElement('p')
+  p.textContent = task.parentElement.parentElement.previousElementSibling.textContent
+  const iconsDiv = document.createElement('div')
+  const trashIcon = document.createElement('i')
+  trashIcon.classList = 'fa-solid fa-trash'
+  trashIcon.id = 'trash'
+  const trashButton = document.createElement('button')
+  trashButton.id = 'delete'
+
+  // Place elements
+  trashButton.append(trashIcon)
+  iconsDiv.append(trashButton)
+  div.append(p)
+  div.append(iconsDiv)
+
+  // Add to the DOM
+  completeTasks.append(div)
+}
+
+function taskHandler(event) {
+  if (event.target.id === 'trash') {
+    removeTask(event.target)
+  } else if (event.target.id === 'check') {
+    markAsDone(event.target)
   }
 }
 
 // Event Listeners
 addButton.addEventListener('click', addTask)
 incompleteTasks.addEventListener('click', taskHandler)
+completeTasks.addEventListener('click', taskHandler)
