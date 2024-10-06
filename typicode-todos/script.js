9fbf5d15c79128a943665d7f57dafda4f10165ba
otavio <proj.otaviocf@gmail.com>
2024-10-05 21:12:57 -0300

    implemented function to delete a task

--- typicode-todos/script.js
+++ typicode-todos/script.js
@@ -2,6 +2,7 @@ const input = document.querySelector('input')
 const addButton = document.querySelector('#add-item')
 const incompleteTasks = document.querySelector('.incomplete-tasks')
 const completeTasks = document.querySelector('.complete-tasks')
+const tasks = document.querySelector('.task')
 
 
 function addTask(event) {
@@ -19,8 +20,10 @@ function addTask(event) {
   trashButton.id = 'delete'
   const checkmarkIcon = document.createElement('i')
   checkmarkIcon.classList = 'fa-solid fa-check'
+  checkmarkIcon.id = 'check'
   const trashIcon = document.createElement('i')
   trashIcon.classList = 'fa-solid fa-trash'
+  trashIcon.id = 'trash'
 
   // Place elements
   iconsDiv.append(checkmarkButton)
@@ -36,6 +39,19 @@ function addTask(event) {
   input.value = ''
 }
 
+function removeTask(task) {
+  task.parentElement.parentElement.parentElement.remove()
+}
+
+function taskHandler(event) {
+  const target = event.target
+  if (event.target.id === 'trash') {
+    removeTask(target)
+  } else if (event.target.id === 'check') {
+
+  }
+}
 
 // Event Listeners
 addButton.addEventListener('click', addTask)
+incompleteTasks.addEventListener('click', taskHandler)
