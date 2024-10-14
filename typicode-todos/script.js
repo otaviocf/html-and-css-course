7d6f1fe2df1bc917519fa881af73309d120206b2
otavio <proj.otaviocf@gmail.com>
2024-10-14 02:58:00 -0300

    reformatted code with updated prettier configuration

--- typicode-todos/script.js
+++ typicode-todos/script.js
@@ -5,136 +5,147 @@ const completeTasks = document.querySelector('.complete-tasks')
 const tasks = document.querySelector('.task')
 const amountIncomplete = document.querySelector('#amount-incomplete')
 const amountCompleted = document.querySelector('#amount-completed')
-const APIurl = 'https://jsonplaceholder.typicode.com/todos'
-
+const APIurl = 'https://jsonplaceholder.typicode.com/todos/'
 
 function addTask(event) {
-  event.preventDefault()
+	event.preventDefault()
 
-  const newTask = {
-    title: input.value,
-    completed: false,
-  }
+	const newTask = {
+		title: input.value,
+		completed: false,
+	}
 
-  fetch(APIurl, {
-    method: 'POST',
-    body: JSON.stringify(newTask),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(res => res.json())
-    .then(data => incompleteTasks.append(createTaskElement(data)))
+	fetch(APIurl, {
+		method: 'POST',
+		body: JSON.stringify(newTask),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => incompleteTasks.append(createTaskElement(data)))
 
-  taskCounter()
+	taskCounter()
 
-  input.value = ''
+	input.value = ''
 }
 
 function createTaskElement(task) {
-  const div = document.createElement('div')
-  div.classList.add('task')
-  div.setAttribute('data-id', task.id)
-  const p = document.createElement('p')
-  p.textContent = task.title
-  const iconsDiv = document.createElement('div')
-  const trashButton = document.createElement('button')
-  trashButton.id = 'delete'
-  const trashIcon = document.createElement('i')
-  trashIcon.classList = 'fa-solid fa-trash'
-  trashIcon.id = 'trash'
+	const div = document.createElement('div')
+	div.classList.add('task')
+	div.setAttribute('data-id', task.id)
+	const p = document.createElement('p')
+	p.textContent = task.title
+	const iconsDiv = document.createElement('div')
+	const trashButton = document.createElement('button')
+	trashButton.id = 'delete'
+	const trashIcon = document.createElement('i')
+	trashIcon.classList = 'fa-solid fa-trash'
+	trashIcon.id = 'trash'
 
-  if (!task.completed) {
-    const checkmarkButton = document.createElement('button')
-    checkmarkButton.id = 'mark-as-done'
-    const checkmarkIcon = document.createElement('i')
-    checkmarkIcon.classList = 'fa-solid fa-check'
-    checkmarkIcon.id = 'check'
+	if (!task.completed) {
+		const checkmarkButton = document.createElement('button')
+		checkmarkButton.id = 'mark-as-done'
+		const checkmarkIcon = document.createElement('i')
+		checkmarkIcon.classList = 'fa-solid fa-check'
+		checkmarkIcon.id = 'check'
 
-    iconsDiv.append(checkmarkButton)
-    checkmarkButton.append(checkmarkIcon)
-  }
+		iconsDiv.append(checkmarkButton)
+		checkmarkButton.append(checkmarkIcon)
+	}
 
-  // Place elements
-  iconsDiv.append(trashButton)
-  trashButton.append(trashIcon)
-  div.append(p)
-  div.append(iconsDiv)
+	// Place elements
+	iconsDiv.append(trashButton)
+	trashButton.append(trashIcon)
+	div.append(p)
+	div.append(iconsDiv)
 
-  return div
+	return div
 }
 
 function removeTask(task) {
-  task.parentElement.parentElement.parentElement.remove()
+	task.parentElement.parentElement.parentElement.remove()
 
-  taskCounter()
+	fetch(
+		APIurl +
+			task.parentElement.parentElement.parentElement.getAttribute('data-id'),
+		{
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	)
+
+	taskCounter()
 }
 
 function markAsDone(task) {
-  // Remove from list of incomplete tasks
-  removeTask(task)
+	// Remove from list of incomplete tasks
+	removeTask(task)
 
-  // Create task element
-  const div = document.createElement('div')
-  div.classList.add('task')
-  const p = document.createElement('p')
-  p.textContent = task.parentElement.parentElement.previousElementSibling.textContent
-  const iconsDiv = document.createElement('div')
-  const trashIcon = document.createElement('i')
-  trashIcon.classList = 'fa-solid fa-trash'
-  trashIcon.id = 'trash'
-  const trashButton = document.createElement('button')
-  trashButton.id = 'delete'
+	// Create task element
+	const div = document.createElement('div')
+	div.classList.add('task')
+	const p = document.createElement('p')
+	p.textContent =
+		task.parentElement.parentElement.previousElementSibling.textContent
+	const iconsDiv = document.createElement('div')
+	const trashIcon = document.createElement('i')
+	trashIcon.classList = 'fa-solid fa-trash'
+	trashIcon.id = 'trash'
+	const trashButton = document.createElement('button')
+	trashButton.id = 'delete'
 
-  // Place elements
-  trashButton.append(trashIcon)
-  iconsDiv.append(trashButton)
-  div.append(p)
-  div.append(iconsDiv)
+	// Place elements
+	trashButton.append(trashIcon)
+	iconsDiv.append(trashButton)
+	div.append(p)
+	div.append(iconsDiv)
 
-  // Add to the DOM
-  completeTasks.append(div)
+	// Add to the DOM
+	completeTasks.append(div)
 
-  taskCounter()
+	taskCounter()
 }
 
 function taskHandler(event) {
-  if (event.target.id === 'trash') {
-    removeTask(event.target)
-  } else if (event.target.id === 'check') {
-    markAsDone(event.target)
-  }
+	if (event.target.id === 'trash') {
+		removeTask(event.target)
+	} else if (event.target.id === 'check') {
+		markAsDone(event.target)
+	}
 }
 
 function taskCounter() {
-  const numberOfIncompleteTasks = incompleteTasks.children.length
-  const numberOfCompletedTasks = completeTasks.children.length
+	const numberOfIncompleteTasks = incompleteTasks.children.length
+	const numberOfCompletedTasks = completeTasks.children.length
 
-  amountCompleted.textContent = numberOfCompletedTasks
-  amountIncomplete.textContent = numberOfIncompleteTasks
+	amountCompleted.textContent = numberOfCompletedTasks
+	amountIncomplete.textContent = numberOfIncompleteTasks
 }
 
 async function fetchTasks(amount) {
-  const response = await fetch(`${APIurl}?_limit=${amount}`)
-  const data = await response.json()
-  
-  return data
+	const response = await fetch(`${APIurl}?_limit=${amount}`)
+	const data = await response.json()
+
+	return data
 }
 
 async function displayFetchData() {
-  const taskList = await fetchTasks(5)
-  
-  taskList.forEach(task => {
-    const taskElement = createTaskElement(task)
-    
-    if (task.completed) {
-      completeTasks.append(taskElement)
-    } else {
-      incompleteTasks.append(taskElement)
-    }
-  });
-  
-  taskCounter()
+	const taskList = await fetchTasks(5)
+
+	taskList.forEach((task) => {
+		const taskElement = createTaskElement(task)
+
+		if (task.completed) {
+			completeTasks.append(taskElement)
+		} else {
+			incompleteTasks.append(taskElement)
+		}
+	})
+
+	taskCounter()
 }
 
 // Event Listeners
